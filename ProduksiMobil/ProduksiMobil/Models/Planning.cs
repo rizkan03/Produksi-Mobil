7feb31c0a3b2808e15e27979fdc1f575009669bc
rizkan03 <61906974+rizkan03@users.noreply.github.com>
2025-12-09 15:17:32 +0700

    Add files via upload

---
+++ ProduksiMobil/ProduksiMobil/Models/Planning.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProduksiMobil.Models
+{
+    public class Planning
+    {
+        public int Id { get; set; }
+        public string InputData { get; set; }
+        public string OutputData { get; set; }
+        public int TotalProduksi { get; set; }
+        public DateTime CreatedDate { get; set; }
+    }
+
+}
